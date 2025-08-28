d28e3ab1b0b48f38e1d6851ee42dbc4aa61e9887
mkdinindu <mkdgangadara0615@gmail.com>
2025-08-28 19:48:50 +0530

    adding things

---
+++ PDSA_CW/src/main/java/com/uhd/PDSA_CW/PDSA_CW/PdsaCwApplication.java
@@ -0,0 +1,13 @@
+package com.uhd.PDSA_CW.PDSA_CW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsaCwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsaCwApplication.class, args);
+	}
+
+}
---
+++ PDSA_CW/src/test/java/com/uhd/PDSA_CW/PDSA_CW/PdsaCwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhd.PDSA_CW.PDSA_CW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsaCwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
