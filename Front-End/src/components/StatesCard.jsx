25fc3947b4d50d387afc2aa100023bcbaa3ff23a
HarinduVihan <harinduvihan@gmail.com>
2025-08-30 14:32:02 +0530

    added some components

---
+++ Front-End/src/components/StatesCard.jsx
@@ -0,0 +1,12 @@
+import "../styles/StatesCard.css";
+
+function StatesCard() {
+  return (
+    <div className="card">
+      <p>Pantry items</p>
+      <p>10</p>
+    </div>
+  );
+}
+
+export default StatesCard;
