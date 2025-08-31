79d1d0f3362d4f01a18f7627652867fb62350d14
haribduvihan <harinduvihan@gmail.com>
2025-08-31 16:04:04 +0530

    styled cards

--- Front-End/src/components/GroceryItemCard.jsx
+++ Front-End/src/components/GroceryItemCard.jsx
@@ -4,10 +4,19 @@ import "../styles/PantryItemCard.css";
 function GroceryItemCard() {
   return (
     <div className="pantry-item-card">
-      <p>Item Name</p>
-      <p>Milk</p>
-      <p>Quantity</p>
-      <p>7</p>
+      <p className="pantry-item-card-name">Apple</p>
+      <div className="pantry-item-card-quantity">
+        <p>
+          <b>Quantity</b>
+        </p>
+        <p>3</p>
+      </div>
+      <div className="pantry-item-card-formattedDate">
+        <p>
+          <b>Expire Date</b>
+        </p>
+        <p>2090-234</p>
+      </div>
     </div>
   );
 }
