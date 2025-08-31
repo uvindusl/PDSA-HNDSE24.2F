d4b0654efcc040e11b49ebafebfc190067ba6ebd
haribduvihan <harinduvihan@gmail.com>
2025-08-31 16:10:28 +0530

    resplved conflict

--- Front-End/src/components/GroceryItemCard.jsx
+++ Front-End/src/components/GroceryItemCard.jsx
@@ -1,21 +1,17 @@
 import { div } from "motion/react-client";
 import "../styles/PantryItemCard.css";
 
-function GroceryItemCard() {
+function GroceryItemCard(props) {
+  const { name, quantity } = props;
+
   return (
     <div className="pantry-item-card">
-      <p className="pantry-item-card-name">Apple</p>
+      <p className="pantry-item-card-name">{name}</p>
       <div className="pantry-item-card-quantity">
         <p>
           <b>Quantity</b>
         </p>
-        <p>3</p>
-      </div>
-      <div className="pantry-item-card-formattedDate">
-        <p>
-          <b>Expire Date</b>
-        </p>
-        <p>2090-234</p>
+        <p>{quantity}</p>
       </div>
     </div>
   );
