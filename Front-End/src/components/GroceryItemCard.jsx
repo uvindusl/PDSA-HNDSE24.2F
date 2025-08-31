3db395f59f99c9990ee4cd0d470984d10923c696
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-08-31 14:15:49 +0530

    make grocery list dyanamic

--- Front-End/src/components/GroceryItemCard.jsx
+++ Front-End/src/components/GroceryItemCard.jsx
@@ -1,13 +1,15 @@
 import { div } from "motion/react-client";
 import "../styles/PantryItemCard.css";
 
-function GroceryItemCard() {
+function GroceryItemCard(props) {
+  const { name, quantity } = props;
+
   return (
     <div className="pantry-item-card">
       <p>Item Name</p>
-      <p>Milk</p>
+      <p>{name}</p>
       <p>Quantity</p>
-      <p>7</p>
+      <p>{quantity}</p>
     </div>
   );
 }
