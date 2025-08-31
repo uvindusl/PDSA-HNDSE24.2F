f6678733e868ec50e94a29a28e036ae13ad6f3be
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-08-31 09:43:00 +0530

    add icon done

--- Front-End/src/components/StatesCard.jsx
+++ Front-End/src/components/StatesCard.jsx
@@ -1,13 +1,21 @@
 import "../styles/StatesCard.css";
-import { Box } from "lucide-react";
+import { Box, TriangleAlert, TrendingUp, ShoppingCart } from "lucide-react";
 
 function StatesCard(props) {
   const { name, count } = props;
   return (
     <div className="card">
-      <Box />
-      <p>{props.name}</p>
-      <p>{props.count}</p>
+      {name === "Pantry Items" ? (
+        <Box />
+      ) : name === "Expired" ? (
+        <TriangleAlert color="#ed333b" />
+      ) : name === "Expiring Soon" ? (
+        <TrendingUp color="#ed333b" />
+      ) : (
+        <ShoppingCart />
+      )}
+      <p>{name}</p>
+      <p>{count}</p>
     </div>
   );
 }
